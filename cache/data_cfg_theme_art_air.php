04748a651fe02b21684421b619fcaae4366ab66d
u19121 <grischenko.n@gmail.com>
2014-07-17 09:59:29 +0400

    Cache update

--- cache/data_cfg_theme_art_air.php
+++ cache/data_cfg_theme_art_air.php
@@ -1,4 +1,4 @@
 <?php exit; ?>
-1436898279
+1437065386
 159
-a:5:{s:4:"name";s:12:"Artodia: Air";s:9:"copyright";s:18:"&copy; Artodia.com";s:7:"version";s:5:"2.0.2";s:14:"parse_css_file";b:1;s:8:"filetime";i:1380687534;}
+a:5:{s:4:"name";s:12:"Artodia: Air";s:9:"copyright";s:18:"&copy; Artodia.com";s:7:"version";s:5:"2.0.2";s:14:"parse_css_file";b:1;s:8:"filetime";i:1405528625;}
--- cache/sql_efd26735c3bcbef398e23fb85e7ccd99.php
+++ cache/sql_efd26735c3bcbef398e23fb85e7ccd99.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1405506814
+1405529987
 SELECT forum_id FROM phpbb_forums WHERE forum_options & 2 <> 0 LIMIT 1
 6
 a:0:{}
