097b489ecbc0ff47267afc7a41ad38538f3783ce
u19121 <grischenko.n@gmail.com>
2014-07-17 13:51:43 +0400

    Adding Readme

--- cache/data_cfg_template_art_air.php
+++ cache/data_cfg_template_art_air.php
@@ -1,4 +1,4 @@
 <?php exit; ?>
-1436898279
+1437126663
 205
-a:6:{s:4:"name";s:12:"Artodia: Air";s:9:"copyright";s:18:"&copy; Artodia.com";s:7:"version";s:5:"2.0.2";s:17:"template_bitfield";s:4:"lNg=";s:12:"inherit_from";s:9:"prosilver";s:8:"filetime";i:1380687534;}
+a:6:{s:4:"name";s:12:"Artodia: Air";s:9:"copyright";s:18:"&copy; Artodia.com";s:7:"version";s:5:"2.0.2";s:17:"template_bitfield";s:4:"lNg=";s:12:"inherit_from";s:9:"prosilver";s:8:"filetime";i:1405528625;}
